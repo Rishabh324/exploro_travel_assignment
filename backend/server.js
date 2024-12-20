c883a7cb76a39da9c5da8f5e655d113b4d7c39b5
Rishabh324 <sinharishabh402@gmail.com>
2024-12-20 14:39:54 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,12 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+app.use(express.json({limit: "50mb"}));
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
 //routes
