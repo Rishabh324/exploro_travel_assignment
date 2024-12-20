0a7331c4244f076357a215519252d262638c4cc2
Rishabh324 <sinharishabh402@gmail.com>
2024-12-20 18:58:16 +0530

    Changes works in local

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import OrganizerDashboard from '../components/OrganizerDashboard'
+// import OrganizerDashboard from '../components/OrganizerDashboard'
 import UserDashboard from '../components/UserDashboard'
 import { useSelector } from 'react-redux';
 import Layout from '../components/Layout';
@@ -8,7 +8,8 @@ const Dashboard = () => {
 
   return (
     <Layout>
-      {user?.role==="user" ? <UserDashboard /> : <OrganizerDashboard />}
+      {/* {user?.role==="user" ? <UserDashboard /> : <OrganizerDashboard />} */}
+      <UserDashboard />
     </Layout>
   )
 }
