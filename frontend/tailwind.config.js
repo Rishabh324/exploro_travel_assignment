add12aa495c7951aeac4e19b331b17d40afe1e09
Rishabh324 <sinharishabh402@gmail.com>
2024-12-21 01:39:30 +0530

    checking ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["hidden", "lay", "absolute", "w-7/12"],
   theme: {
     extend: {},
   },
